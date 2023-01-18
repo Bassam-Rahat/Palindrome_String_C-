827d06380cbf738a0afc9af3a12f1698f7435a25
Bassam Rahat <bassamrahat.ibs@gmail.com>
2023-01-18 10:20:02 +0500

    first commit

---
+++ Palindrome_String/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string: ");
+            string str = Console.ReadLine();
+
+            checkPalindrome(str);
+            Console.ReadLine();
+        }
+
+        public static void checkPalindrome(string str)
+        {
+            string result= string.Empty;
+            for(int i=str.Length-1; i>=0; i--)
+            {
+                result += str[i];
+            }
+
+            if(result == str)
+            {
+                Console.WriteLine("String is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("String is not Palindrome");
+            }
+        }
+    }
+}
